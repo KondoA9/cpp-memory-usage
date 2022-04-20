2136dea0386885289618c4c2c1e1286a503a647f
KondoA9 <kondo.eque@gmail.com>
2022-04-20 20:01:48 +0900

    implement GetProcessMemoryUsage for mac

--- include/memuse/memuse_mac.hpp
+++ include/memuse/memuse_mac.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <mach/mach.h>
+#include <sys/sysctl.h>
+
 namespace a9 {
     namespace memory {
         // Return memory usage [%]
@@ -9,12 +12,28 @@ namespace a9 {
 
         // Return total physical memory [byte]
         size_t GetTotalPhysicalMemory() {
-            return 0;
+            int mib[2]    = {CTL_HW, HW_MEMSIZE};
+            int64_t total = 0;
+            size_t length = sizeof(total);
+
+            sysctl(mib, 2, &total, &length, NULL, 0);
+
+            return static_cast<size_t>(total);
         }
 
-        // Return private memory usage [byte]
+        // Return RSS [byte]
+        // The value may be incorrect
         size_t GetProcessMemoryUsage() {
-            return 0;
+            task_t targetTask = mach_task_self();
+            task_basic_info ti;
+            mach_msg_type_number_t count = TASK_BASIC_INFO_64_COUNT;
+
+            const auto result = task_info(targetTask, TASK_BASIC_INFO_64, (task_info_t)&ti, &count);
+            if (result == KERN_SUCCESS) {
+                return ti.resident_size;
+            } else {
+                return 0;
+            }
         }
     }
 }
