e278ac1292e2e2c6677573a759ffe27a27337709
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2022-04-22 10:46:02 +0900

    add version macro

--- include/memuse.hpp
+++ include/memuse.hpp
@@ -1,5 +1,12 @@
 #pragma once
 
+#define CPP_MEMORY_USAGE_VERSION_MAJOR 0
+#define CPP_MEMORY_USAGE_VERSION_MINOR 1
+#define CPP_MEMORY_USAGE_VERSION_PATCH 1
+#define CPP_MEMORY_USAGE_VERSION_STRING "0.1.1"
+#define CPP_MEMORY_USAGE_VERSION                                                                                       \
+    (CPP_MEMORY_USAGE_VERSION_MAJOR * 10000 + CPP_MEMORY_USAGE_VERSION_MINOR * 100 + CPP_MEMORY_USAGE_VERSION_PATCH)
+
 #if _WIN32 || WIN64
 #include "memuse/memuse_win.hpp"
 #elif __MACH__
